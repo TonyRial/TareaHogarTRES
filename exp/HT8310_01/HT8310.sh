da8d6bed84b0cfb0e835f988c01c35ba67c92a98
Antonio Rial <95229695+TonyRial@users.noreply.github.com>
2022-05-13 23:40:15 -0300

    Add files via upload

---
+++ exp/HT8310_01/HT8310.sh
@@ -0,0 +1,13 @@
+tabulador="	"
+exp_name=HT8310
+echo "experiment	timestamp	event"  >  log.txt 
+fecha0=$(date +"%Y%m%d %H%M%S") 
+echo "$exp_name""$tabulador""$fecha0""$tabulador""SH_START" >> log.txt 
+Rscript --vanilla ~/labo/src/completo/z931_HT_lightgbm.r  HT8310  2>&1 | tee outfile 
+fecha1=$(date +"%Y%m%d %H%M%S") 
+echo "$exp_name""$tabulador""$fecha1""$tabulador""SH_END" >> log.txt 
+find ./ ! -name "*.gz" ! -name . -exec cp -prt /media/expshared/hecaeventos/exp/HT8310/  {} +  
+
+#suicidio
+export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google') 
+export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google') 
