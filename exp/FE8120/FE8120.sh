3b9df0b3fbb9c236df89b48fc6cd39056ef71d22
Antonio Rial <95229695+TonyRial@users.noreply.github.com>
2022-05-13 23:59:13 -0300

    Add files via upload

---
+++ exp/FE8120/FE8120.sh
@@ -0,0 +1,13 @@
+tabulador="	"
+exp_name=FE8120
+echo "experiment	timestamp	event"  >  log.txt 
+fecha0=$(date +"%Y%m%d %H%M%S") 
+echo "$exp_name""$tabulador""$fecha0""$tabulador""SH_START" >> log.txt 
+Rscript --vanilla ~/labo/src/completo/z912_FE_desborde.r  FE8120  2>&1 | tee outfile 
+fecha1=$(date +"%Y%m%d %H%M%S") 
+echo "$exp_name""$tabulador""$fecha1""$tabulador""SH_END" >> log.txt 
+find ./ ! -name "*.gz" ! -name . -exec cp -prt /media/expshared/hecaeventos/exp/FE8120/  {} +  
+
+#suicidio
+export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google') 
+export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google') 
